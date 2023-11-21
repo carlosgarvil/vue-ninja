f9c8f4ebcc8255b637749103b0992b0d90d1d7bc
Carlos GV <carlos.garcia@iespoligonosur.org>
2023-11-21 22:02:22 +0100

    until timer

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import Block from './components/Block.vue'
+export default{
+  name: 'App',
+  components: { Block},
+  data(){
+    return{
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+
+    }
+}
+}
+// https://youtu.be/bc6czIBLKTg?si=psJZ8yCXRqnpkIyk&t=1406
+</script>
+
+<template>
+  <h1>Ninja Vue</h1>
+  <div>
+    <button @click="start" :disabled="isPlaying">Play</button>
+  </div>
+  <Block v-if="isPlaying" :delay="delay"></Block>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="block" v-if="showBlock" @click="">
+        Click me!
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['delay'],
+        data(){
+            return{
+                showBlock: false
+            }
+        },
+        mounted(){
+            //console.log('mounted')    
+            setTimeout(() => {
+                    this.showBlock = true
+                }, this.delay)
+        },
+        updated(){
+            //console.log('updated')
+        },
+        unmounted(){
+        //  console.log('unmounted')
+        }
+    }
+</script>
+<style>
+.block{
+    width: 400px;
+    height: 400px;
+    background-color: green;
+    margin: 10px;
+    display: inline-block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 15px;
+    color: white;
+}
+</style>
